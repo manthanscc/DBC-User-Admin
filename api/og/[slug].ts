d47ffc80452a1b170b1fae44b7260fcb2d3512bd
yashvavaliya <iamcoder112004@gmail.com>
2025-09-12 14:30:56 +0530

    update: slug for supabaseurl in env

--- api/og/[slug].ts
+++ api/og/[slug].ts
@@ -1,11 +1,12 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.VITE_SUPABASE_URL || process.env.NEXT_PUBLIC_SUPABASE_URL || process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.VITE_SUPABASE_ANON_KEY || process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || process.env.SUPABASE_ANON_KEY;
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Supabase URL or Anon Key is not defined in environment variables');
+  throw new Error('SUPABASE_URL or SUPABASE_ANON_KEY is not defined in environment variables');
 }
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
