147803766e965003d2b59922fdb46b8d99f28674
yashvavaliya <iamcoder112004@gmail.com>
2025-11-06 16:31:59 +0530

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,33 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+    }),
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+        },
+      },
     },
   },
 });
